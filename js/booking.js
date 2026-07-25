f5db65d96d5718a2457831796b7a55b91bd44401
wildanichlasulamal2-create <wildanichlasulamal2@gmail.com>
2026-07-25 11:19:02 +0700

    Update booking.js

--- js/booking.js
+++ js/booking.js
@@ -1,1 +1,33 @@
+const treatment=document.getElementById("treatment");
 
+const harga=document.getElementById("harga");
+
+treatment.addEventListener("change",()=>{
+
+switch(treatment.value){
+
+case "Massage Relax":
+
+harga.value="Rp150.000";
+
+break;
+
+case "Facial Treatment":
+
+harga.value="Rp120.000";
+
+break;
+
+case "Body Scrub":
+
+harga.value="Rp175.000";
+
+break;
+
+default:
+
+harga.value="";
+
+}
+
+});
