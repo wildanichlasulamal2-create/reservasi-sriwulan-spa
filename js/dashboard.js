079303ef44c9705f0789021a057d6b1e4cd70830
wildanichlasulamal2-create <wildanichlasulamal2@gmail.com>
2026-07-28 10:30:54 +0700

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -15,3 +15,93 @@ document.getElementById("totalPendapatan").innerHTML = "Rp2.450.000";
 document.getElementById("totalPelanggan").innerHTML = 126;
 
 document.getElementById("kehadiran").innerHTML = "8 / 10";
+
+// ============================
+// GRAFIK RESERVASI
+// ============================
+
+new Chart(document.getElementById("reservasiChart"),{
+
+type:"line",
+
+data:{
+
+labels:["Sen","Sel","Rab","Kam","Jum","Sab","Min"],
+
+datasets:[{
+
+label:"Reservasi",
+
+data:[8,12,10,15,18,20,14],
+
+fill:false,
+
+borderWidth:3,
+
+tension:.4
+
+}]
+
+},
+
+options:{
+
+responsive:true,
+
+plugins:{
+
+legend:{
+
+display:false
+
+}
+
+}
+
+}
+
+});
+
+
+
+// ============================
+// GRAFIK PENDAPATAN
+// ============================
+
+new Chart(document.getElementById("pendapatanChart"),{
+
+type:"bar",
+
+data:{
+
+labels:["Sen","Sel","Rab","Kam","Jum","Sab","Min"],
+
+datasets:[{
+
+label:"Pendapatan",
+
+data:[1200000,1500000,1800000,2000000,1750000,2500000,1900000],
+
+borderWidth:1
+
+}]
+
+},
+
+options:{
+
+responsive:true,
+
+plugins:{
+
+legend:{
+
+display:false
+
+}
+
+}
+
+}
+
+});
