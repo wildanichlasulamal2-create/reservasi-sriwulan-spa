f2a0a0224e14d0a96fde5d22a694e238b85517c7
wildanichlasulamal2-create <wildanichlasulamal2@gmail.com>
2026-07-28 10:15:52 +0700

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,9 @@
+const hari = new Date();
 
+document.getElementById("tanggal").innerHTML =
+hari.toLocaleDateString("id-ID",{
+    weekday:"long",
+    day:"numeric",
+    month:"long",
+    year:"numeric"
+});
